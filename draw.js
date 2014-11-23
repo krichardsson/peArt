e6ef1fb076d92376eaba86fa59f64e650b080c2e
Kristoffer Richardsson <kristoffer@richardsson.org>
2014-11-23 13:44:45 +0100

    Refactoring

--- draw.js
+++ draw.js
@@ -1,6 +1,7 @@
-width = 512;
-height = 512;
-maxRadius = 100;
+centerMin = 100;
+centerMax = 600;
+radiusMin = 1;
+radiusMax = 100;
 
 //e = [2, 7, 1, 8, 2, 8, 1, 8, 2, 8, 4, 5, 9, 0, 4, 5, 2, 3, 5, 3, 6, 0, 2, 8, 7, 4, 7, 1, 3, 5, 2, 6, 6, 2, 4, 9, 7, 7, 5, 7, 2, 4, 7, 0, 9, 3, 6, 9, 9, 9, 5, 9, 5, 7, 4, 9, 6, 6, 9, 6, 7, 6, 2, 7, 7, 2, 4, 0, 7, 6, 6, 3, 0, 3, 5, 3];
 //p = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3, 8, 3, 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7, 1, 6, 9, 3, 9, 9, 3, 7, 5, 1, 0, 5, 8, 2, 0, 9, 7, 4, 9, 4, 4, 5, 9, 2, 3, 0, 7, 8, 1, 6, 4, 0, 6, 2, 8, 6];
@@ -13,13 +14,13 @@ function start() {
         e = getE(i);
         p = getPi(i);
 
-        x = (e[0] * 100 + e[1] * 10 + e[2]) * width / 1000;
-        y = (p[0] * 100 + p[1] * 10 + p[2]) * height / 1000;
-        radius = (e[2] * 100 + p[1] * 10 + e[0]) * maxRadius / 1000;
+        x = limit(e[0] * 100 + e[1] * 10 + e[2], 1000, centerMin, centerMax);
+        y = limit(p[0] * 100 + p[1] * 10 + p[2], 1000, centerMin, centerMax);
+        radius = limit(e[2] * 100 + p[1] * 10 + e[0], 1000, radiusMin, radiusMax);
 
-        r = (e[0] * 100 + p[0] * 10 + e[1]) / 4;
-        g = (e[1] * 100 + p[2] * 10 + p[0]) / 4;
-        b = (e[2] * 100 + p[1] * 10 + p[2]) / 4;
+        r = limit(e[1] * 100 + p[2] * 10 + e[0], 1000, 0, 255);
+        g = limit(p[1] * 100 + e[2] * 10 + p[0], 1000, 0, 255);
+        b = limit(e[1] * 100 + e[2] * 10 + p[0], 1000, 0, 255);
 
         //alert("r:" + r + ", g:" + g + ", b:" + b + ", x:" + x + ", y:" + y + ", radius:" + radius);
         circle(ctx, r, g, b, x, y, radius);
@@ -51,7 +52,7 @@ function circle(ctx, r, g, b, x, y, radius) {
 }
 
 function getE(i) {
-    return [rndSingleDigit(), rndSingleDigit(), rndSingleDigit()];
+    return [rndSingleDigit(), rndSingleDigit(), rndSingleDigit(), rndSingleDigit(), rndSingleDigit(), rndSingleDigit(), rndSingleDigit(), rndSingleDigit(), rndSingleDigit()];
 }
 
 function getPi(i) {
@@ -60,4 +61,12 @@ function getPi(i) {
 
 function rndSingleDigit() {
     return Math.floor(Math.random() * 10);
+}
+
+function limit(value, valueMax, outMin, outMax) {
+    return (value * (outMax - outMin) / valueMax) + outMin;
+}
+
+function composeFromArray(array, index) {
+    
 }
